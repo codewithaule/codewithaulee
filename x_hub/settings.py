8c7a61e196d88703dd054bce1ab97e9223b8cb82
codewithaule <aulekator@gmail.com>
2022-04-03 09:27:55 +0100

    my changes

--- x_hub/settings.py
+++ x_hub/settings.py
@@ -12,8 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 
 from pathlib import Path
-import django_heroku
-import dj_database_url
+ 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,7 +28,7 @@ SECRET_KEY = 'django-insecure-4)p%9b=dfc9%=lv1yt%e^5)gg$sjg13ktqa24-e(f7%xu&+jdi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -126,7 +125,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
-django_heroku.settings(local())
+ 
  
  
 MEDIA_URL = '/media/'
